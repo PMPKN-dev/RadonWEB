62790d3dbad7572ccc5c902eac3583ae84f8fd18
PMPKN-dev <michal-janczak@outlook.com>
2023-06-08 12:23:31 +0200

    Table.html and index.html "work"

---
+++ js/table.js
@@ -0,0 +1,53 @@
+const APIRoot = 'https://localhost:7168/'
+
+
+async function populateTables() {
+
+    let url = APIRoot+'api/Customers/'
+
+    await fetch(url)
+        .then((res)=>res.json())
+        .then(function (json){
+            let i=0;
+            while(i<json.length)
+            {
+                let j = 0
+                let dataloggers = []
+                while(j<json[i].dataloggers.length)
+                {
+                    dataloggers[j] = json[i].dataloggers[j].serialnumber
+                    j++
+                }
+
+                createTableElement(
+                    json[i].id,
+                    json[i].email,
+                    dataloggers.toString().replace(',','; ')
+                )
+                i++
+            }
+        })
+}
+
+function createTableElement(ID,email,loggers) {
+    let table = document.getElementById('TableBody')
+
+    let row = document.createElement('tr')
+
+    let IDCol = document.createElement('td')
+    IDCol.setAttribute('style','background: mintcream')
+    IDCol.innerHTML = ID
+
+    let emailCol = document.createElement('td')
+    emailCol.setAttribute('style','background: mintcream')
+    emailCol.innerHTML = email
+
+    let loggersCol = document.createElement('td')
+    loggersCol.setAttribute('style','background: mintcream')
+    loggersCol.innerHTML = loggers
+
+    row.append(IDCol,emailCol,loggersCol)
+    table.append(row)
+
+
+}
