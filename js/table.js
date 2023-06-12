f69461b64e75ad87be150f45ba6c2125a3c4db94
PMPKN-dev <michal-janczak@outlook.com>
2023-06-12 11:15:55 +0200

    Made table only load 10 entries at a time, added page functionality in table

--- js/table.js
+++ js/table.js
@@ -1,32 +1,35 @@
 const APIRoot = 'https://localhost:7168/'
 
+let PageNum = 0;
+
 
 async function populateTables() {
 
     let url = APIRoot+'api/Customers/'
     document.getElementById('TableBody').replaceChildren()
 
+    document.getElementById('currentPageNum').innerHTML = (PageNum+1).toString()
 
 
-    if(document.cookie.substring(13)!=='true'){
-        return
-    }
+    if(document.cookie.substring(13)!=='true'){ return }
+
     await fetch(url)
         .then((res)=>res.json())
         .then(function (json){
-            let i=0;
+
+            let i= PageNum * 10; //customer iterator
+
             while(i<json.length)
             {
-                let j = 0
+                let j = 0 //loggers iterator
                 let dataloggers = []
-                while(j<json[i].dataloggers.length)
+                while(j<json[i].dataloggers.length) //creates a composite element to display all loggers
                 {
                     dataloggers[j] = json[i].dataloggers[j].serialnumber
                     j++
                 }
 
-                if(json[i].id === '6481afd3af2dcae42ea7369c'){
-                    //skip
+                if(json[i].id === '6481afd3af2dcae42ea7369c'){ //skips admin ID
                 } else{
                 createTableElement(
                     json[i].id,
@@ -36,6 +39,37 @@ async function populateTables() {
                 }i++
             }
         })
+}
+
+function prevPage(){
+    PageNum -=1
+    populateTables()
+}
+
+function nextPage(){
+    PageNum +=1
+    populateTables()
+}
+
+async function countPages(){
+
+    let url = APIRoot+'api/Customers/'
+
+    let i = 0
+
+
+    await fetch(url)
+        .then((res)=>res.json())
+        .then(function (json){
+            while(i<json.length)
+            {
+                i++
+            }
+        })
+
+    document.getElementById('totalPageNum').innerHTML = (Math.round(i/10)+1).toString()
+
+
 }
 
 function createTableElement(ID,email,loggers) {
