6e9ad83c8b056007f6a3e5238b6295ba105211ad
PMPKN-dev <michal-janczak@outlook.com>
2023-06-13 09:11:13 +0200

    Customer page functionality added, ID hardcoded for now

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,5 @@
-const APIRoot = 'https://localhost:7168/api'
+const APIRoot = 'https://localhost:7168/api/'
+const customerID = '648812cefc100840f964d879'
 
 async function populateData(){
     //async call to load info
@@ -13,29 +14,52 @@ async function populateData(){
 async function loadCustomerInfo(){
     let email = document.getElementById('customerEmail')
 
-    email.innerHTML = 'placeholder text'
+    let url = APIRoot+'Customers/'+customerID
+
+    await fetch(url)
+        .then((res) => res.json())
+        .then(function (json) {
+            email.innerHTML = json.email
+        })
+
 }
 
 async function populateTable(){
-    createTableElement('testing','the waters')
+
+    let url = APIRoot+'Customers/'+customerID
+
+    await fetch(url)
+        .then((res)=>res.json())
+        .then(function (json){
+
+            for(let i = 0; i<json.dataloggers.length;i++) {
+                createTableElement(json.dataloggers[i].id,json.dataloggers[i].location,json.dataloggers[i].name)
+            }
+
+        })
+
 }
 
 
-function createTableElement(first,second) {
+function createTableElement(first,second,third) {
     let table = document.getElementById('tableBody')
 
     let row = document.createElement('tr')
 
     let firstCol = document.createElement('td')
     firstCol.setAttribute('style','background: mintcream')
-    firstCol.innerHTML = second
+    firstCol.innerHTML = first
 
     let secondCol = document.createElement('td')
     secondCol.setAttribute('style','background: mintcream')
-    secondCol.innerHTML = first
+    secondCol.innerHTML = second
+
+    let thirdCol = document.createElement('td')
+    thirdCol.setAttribute('style','background: mintcream')
+    thirdCol.innerHTML = third
 
 
-    row.append(firstCol ,secondCol)
+    row.append(firstCol ,secondCol,thirdCol)
     table.append(row)
 }
 
